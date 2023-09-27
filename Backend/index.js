1ba0a64ed81259afec671abba71e3af15d869670
shivamvr <shivamweb2@gmail.com>
2023-09-27 22:30:14 +0530

    backend first commit

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+
+
+const express = require('express')
+const {userRouter} = require('./routes/user.Routes')
+const cors = require('cors')
+const {connection} = require('./db')
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.status(200).send({"msg":"this is the homepage"})
+})
+
+
+app.use('/users',userRouter)
+
+app.listen(8080, async()=>{
+     try{
+         await connection
+         console.log('Connected to DB')
+         console.log('Server is running at port 8080')
+     }catch(err){
+        console.log("Error:",err)
+     }
+})
