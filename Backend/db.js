8841c9eb05af1ef50a134281348adcd1f7cace9a
shivamvr <shivamweb2@gmail.com>
2023-09-30 12:40:43 +0530

    day-4

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const connection = mongoose.connect(process.env.MONGO_URL)
-// const connection = mongoose.connect(process.env.MONGO_URL)
 
 
 module.exports = {connection}
